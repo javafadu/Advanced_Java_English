4fd473efbb1d90e27ef4de8a652fe9de756f60db
javafadu <javafadu@gmail.com>
2023-05-26 00:11:08 +0300

    26May-0010

--- src/Threads/MultiThreading01.java
+++ src/Threads/MultiThreading01.java
@@ -23,7 +23,7 @@ public class MultiThreading01 {
         counter3.join();  // wait until counter3 thread finishes
         counter4.join();  // wait until counter4 thread finishes
         long endTime2 = System.currentTimeMillis();
-        System.out.println("Wit Multi Thread Time : "+(endTime2-startTime2));
+        System.out.println("With Multi Thread Time : "+(endTime2-startTime2));
 
     }
 

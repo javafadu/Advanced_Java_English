462f0592c3557599f745b1572e7a6c22efb88b32
javafadu <javafadu@gmail.com>
2023-06-21 23:20:58 +0300

    21Jun-2320

---
+++ src/AdvancePractice/multithreads/threadPool/AppointmentApp2.java
@@ -0,0 +1,32 @@
+package AdvancePractice.multithreads.threadPool;
+
+/*
+NOTES:
+    - Working with thread is expensive, each thread requires / needs sources on the system
+    - These sources are CPU, RAM, etc.
+    - We can optimize and limit using threads for source efficiency
+
+    *****
+    For above reason, we can create a thread pool with limited capacity,
+    we can fill these thread pool with created threads which are already created before and ready to use
+    -> This way provides us more performance with less sources (efficiency)
+
+ */
+
+
+/*
+TASK :
+    Create an appointment app
+    The application provides next day availability for each request.
+    total application number is : 5
+    create a thread pool and use 3 threads
+ */
+public class AppointmentApp2 {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+}
---
+++ src/AdvancePractice/multithreads/threadPool/AppointmentCenter2.java
@@ -0,0 +1,14 @@
+package AdvancePractice.multithreads.threadPool;
+
+import java.time.LocalDate;
+
+public class AppointmentCenter2 {
+
+    private LocalDate day = LocalDate.now();
+
+    public synchronized LocalDate getAppointmentDate() {
+        day= day.plusDays(1);
+        return day;
+    }
+
+}
